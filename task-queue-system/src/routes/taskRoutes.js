6a01eb581fa6bb05fce91af592db884e69251f19
atreeverma <atreeverma17@gmail.com>
2026-04-15 16:55:24 +0530

    m

--- task-queue-system/src/routes/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createTask } from "../controllers/taskController"
-
-const router = express.Router();
-
-router.post('/task',createTask);
-
-export default router;
--- task-queue-system/src/workers/worker.js
+++
@@ -1,8 +0,0 @@
-import { connectQueue,getChannel,QUEUE } from "../config/rabbitmq"
-async function startWorker() {
-    await connectQueue()
-
-    const channel = getChannel()
-
-    
-}
